37f07730ea5725e484c98421d0154c29531dc56d
Pedro Henrique Gomes Lima <pedrohenriquegomeslima@alu.ufc.br>
2024-06-01 10:50:55 -0300

    ajustando a controller

---
+++ backend/controllers/movieController.js
@@ -0,0 +1,78 @@
+import movieService from "../services/movieServices.js";
+const movieServices = new movieService();
+
+export async function getMovies(_, res) {
+    try {
+        const movies = await movieServices.getAll();
+        res.status(200).json({ movies });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ err: error });
+    }
+}
+
+export async function createMovie(req, res) {
+    const result = movieServices.createMovies([req.body.title, req.body.plot, req.body.released, req.body.runtime, req.body.gender, req.body.director, req.body.poster, req.body.writer, req.body.country, req.body.awards, req.body.imdbRating, req.body.price]);
+    res.json({
+        msg: result
+    })
+}
+
+export function deleteMovie(req, res) {
+    const movieId = req.body.id;
+    if (!movieId) {
+        res.status(400).json({ msg: 'ID do filme não fornecido' });
+        return;
+    }
+
+    movieServices.deleteMovie(movieId).then(msg => {
+        res.json({
+            msg: msg
+        });
+    }).catch(error => {
+        res.status(500).json({
+            msg: 'Erro ao deletar o usuário',
+            error: error
+        });
+    });
+}
+
+export async function viewMovie(req, res) {
+    try {
+        const movieId = req.params.id; 
+        const movie = await movieServices.viewProfile(parseInt(movieId));
+        res.status(200).json({movie: movie,
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({err: error,
+        })
+    }
+}
+
+export async function updateMovie(req, res) {
+    try {
+        const movie = await movieServices.updateMovie(
+           [
+            req.body.title,
+            req.body.plot,
+            req.body.released,
+            req.body.runtime,
+            req.body.gender,
+            req.body.director,
+            req.body.poster,
+            req.body.writer,
+            req.body.country,
+            req.body.awards,
+            req.body.imdbRating,
+            req.body.price,
+            req.params.id, 
+           ]
+        )
+        res.status(200).json({movie: movie })
+    } catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+}
---
+++ backend/database/database.js
@@ -0,0 +1,30 @@
+import sqlite3 from 'sqlite3';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const dbPath = path.resolve(__dirname, 'instance/database_Production.db');
+
+
+export class DatabaseConnection {
+    static connection = null;
+
+    constructor() {
+        if (DatabaseConnection.connection) {
+            throw new Error("Use DatabaseConnection.getDbConnection() to get the single instance of this class.");
+        }
+        DatabaseConnection.connection = new sqlite3.Database(dbPath);
+    }
+
+    static getDbConnection() {
+        if (!DatabaseConnection.connection) {
+            new DatabaseConnection();
+        }
+        return DatabaseConnection.connection;
+    }
+}
